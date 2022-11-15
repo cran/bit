02ef361a415bb7268767da034fb56c0018b59e1f
Jens Oehlschlägel <Jens.Oehlschlaegel@truecluster.com>
2022-11-15 20:20:16 +0000

    version 4.0.5

--- R/bit-package.R
+++ R/bit-package.R
@@ -18,12 +18,31 @@ NULL
 
 # devtools::use_vignette("bit-usage")
 # devtools::use_vignette("bit-performance")
-## require(rhub)
-## rhub_bit_4.0.3 <- check_for_cran(
-##   path = "../bit_4.0.3.tar.gz"
-## , email = "Jens.Oehlschlaegel@truecluster.com"
-## , check_args = "--as-cran"
-## , env_vars = c('_R_CHECK_FORCE_SUGGESTS_'= "false",'_R_CHECK_CRAN_INCOMING_USE_ASPELL_'= "true", '_R_CHECK_XREFS_MIND_SUSPECT_ANCHORS_'="true")
-## , platforms = NULL
-## , show_status = FALSE
-## )
+
+# require(rhub)
+# rhub_bit_4.0.5 <- check_for_cran(
+#   path = "../bit_4.0.5.tar.gz"
+# , email = "Jens.Oehlschlaegel@truecluster.com"
+# , check_args = "--as-cran"
+# , env_vars = c('_R_CHECK_FORCE_SUGGESTS_'= "false",'_R_CHECK_CRAN_INCOMING_USE_ASPELL_'= "true", '_R_CHECK_XREFS_MIND_SUSPECT_ANCHORS_'="true")
+# , platforms = NULL
+# , show_status = FALSE
+# )
+# 
+# ─  Uploading package
+# ─  Preparing build, see status at
+# https://builder.r-hub.io/status/bit_4.0.5.tar.gz-b6a1c13ef92c42d9b68257139666fb46
+# https://builder.r-hub.io/status/bit_4.0.5.tar.gz-abc80afc9741404c9e86511dee4585c5
+# https://builder.r-hub.io/status/bit_4.0.5.tar.gz-93409d19ee544acab83be7de1b380740
+# https://builder.r-hub.io/status/bit_4.0.5.tar.gz-d4f141780d004fa388c82d027a6e40e2
+
+# olddir <- "../revdepold"
+# newdir <- "../revdepnew"
+# tools::check_packages_in_dir(olddir,
+#                              check_args = c("--as-cran", ""),
+#                              reverse = list(repos = getOption("repos")["CRAN"]))
+# tools::check_packages_in_dir(newdir, old=olddir
+#                              check_args = c("--as-cran", ""),
+#                              reverse = list(repos = getOption("repos")["CRAN"]))
+# tools::summarize_check_packages_in_dir_results(newdir, all = FALSE, full = TRUE)
+# tools::check_packages_in_dir_changes(newdir, olddir, outputs = TRUE, sources = FALSE)
