fb22024066917c718b2773953ba98cd9a3eab116
Jens Oehlschlägel <Jens.Oehlschlaegel@truecluster.com>
2024-09-20 15:20:09 +0000

    version 4.5.0

--- R/bit-package.R
+++ R/bit-package.R
@@ -19,23 +19,6 @@ NULL
 # devtools::use_vignette("bit-usage")
 # devtools::use_vignette("bit-performance")
 
-# require(rhub)
-# rhub_bit_4.0.5 <- check_for_cran(
-#   path = "../bit_4.0.5.tar.gz"
-# , email = "Jens.Oehlschlaegel@truecluster.com"
-# , check_args = "--as-cran"
-# , env_vars = c('_R_CHECK_FORCE_SUGGESTS_'= "false",'_R_CHECK_CRAN_INCOMING_USE_ASPELL_'= "true", '_R_CHECK_XREFS_MIND_SUSPECT_ANCHORS_'="true")
-# , platforms = NULL
-# , show_status = FALSE
-# )
-# 
-# ─  Uploading package
-# ─  Preparing build, see status at
-# https://builder.r-hub.io/status/bit_4.0.5.tar.gz-b6a1c13ef92c42d9b68257139666fb46
-# https://builder.r-hub.io/status/bit_4.0.5.tar.gz-abc80afc9741404c9e86511dee4585c5
-# https://builder.r-hub.io/status/bit_4.0.5.tar.gz-93409d19ee544acab83be7de1b380740
-# https://builder.r-hub.io/status/bit_4.0.5.tar.gz-d4f141780d004fa388c82d027a6e40e2
-
 # olddir <- "../revdepold"
 # newdir <- "../revdepnew"
 # tools::check_packages_in_dir(olddir,
